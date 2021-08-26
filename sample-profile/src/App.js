d08ac57c5c23d8a3c55983696f2ea952911480d2
Abdollah Ebadi <abdi.eb79@gmail.com>
2021-08-27 02:06:57 +0430

    Debugged and created dashboard

--- sample-profile/src/App.js
+++ sample-profile/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Dashboard from "./Components/Dashboard/Dashboard";
 import Footer from "./Components/Footer/Footer";
 import NavBar from "./Components/NavBar/NavBar";
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div>
       <NavBar />
+      <Dashboard />
       <Footer />
     </div>
   );
---
+++ sample-profile/src/Components/Dashboard/Dashboard.jsx
@@ -0,0 +1,13 @@
+import "./Dashboard.css";
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard">
+      <div className="personal-info row">
+        <div className=""></div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
