2716e149bc27aab98c0e0a16e256a5b3e21c4e6e
Abdollah Ebadi <abdi.eb79@gmail.com>
2021-08-27 01:38:05 +0430

    Revert "Worked on navBar"
    This reverts commit 58dfafbe46a9b186e7ff99ef9e4a95392a2433f9.

--- sample-profile/src/Components/NavBar/NavBar.jsx
+++ sample-profile/src/Components/NavBar/NavBar.jsx
@@ -1,9 +1,6 @@
 import { useState } from "react";
-import "./NavBar.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import logo from "../../assets/icons/image002.jpg";
-import { CgProfile } from "react-icons/cg";
-import { BsBell } from "react-icons/bs";
+import styles from "./NavBar.module.css";
+// import logo from "../../../assets/icons/image002.jpg";
 const NavBar = () => {
   const [navBarItems, setNavBarItems] = useState([
     { id: 1, title: "چطور کار می‌کند؟" },
@@ -11,38 +8,13 @@ const NavBar = () => {
     { id: 3, title: "سوالات پرتکرار" },
     { id: 4, title: "درباره ما" },
   ]);
-
-  const [userName, setUserName] = useState("آریوبرزن");
   return (
-    <div className="navBar row">
-      <div className="logo col-xl-3 col-lg-3 col-md-">
-        <img alt="logo" src={logo} />
-      </div>
-      <div className="items col-xl-6 col-lg-6 col-md-8">
+    <div className={styles.navBar}>
+      <div className={styles.logo}>{/* <img alt="logo" src={logo} /> */}</div>
+      <div className={styles.items}>
         {navBarItems.map((item) => {
-          return (
-            <a href="#" key={item.id}>
-              {item.title}
-            </a>
-          );
+          return <h4 key={item.id}>{item.title}</h4>;
         })}
-      </div>
-      <div className="details col-xl-3 col-lg-3 col-md-2">
-        <div className="row detail w-100">
-          <div className="col name">
-            <div className="row">
-              <span className="col-2">
-                <CgProfile />
-              </span>
-              <span className="col-10 d-flex justify-content-center">
-                {userName}
-              </span>
-            </div>
-          </div>
-          <div className="col notification">
-            <BsBell />
-          </div>
-        </div>
       </div>
     </div>
   );
