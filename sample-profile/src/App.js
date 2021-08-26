0f77986c5db31ab10fc82ed00e521529ed47657e
Abdollah Ebadi <abdi.eb79@gmail.com>
2021-08-27 01:37:23 +0430

    Revert "Worked on Footer"
    This reverts commit 866aadb74f5c63d39d8ead099434ddd748ecab1e.

--- sample-profile/src/App.js
+++ sample-profile/src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
-import Footer from "./Components/Footer/Footer";
 import NavBar from "./Components/NavBar/NavBar";
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Footer />
     </div>
   );
 }
