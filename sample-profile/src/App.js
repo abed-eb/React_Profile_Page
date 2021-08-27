a4cc1b926780dd7f59635e4c7df520f1351a0814
Abdollah Ebadi <abdi.eb79@gmail.com>
2021-08-27 12:54:42 +0430

    Worked on dashboard

--- sample-profile/src/App.js
+++ sample-profile/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Dashboard from "./Components/Dashboard/Dashboard";
 import Footer from "./Components/Footer/Footer";
 import NavBar from "./Components/NavBar/NavBar";
@@ -7,7 +8,13 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Dashboard />
+      <div className="row">
+        <div className="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3">
+          <Dashboard />
+        </div>
+        <div className="col-xl-9 col-lg-9 col-md-9 col-sm-9 col-9 w-100"></div>
+      </div>
+
       <Footer />
     </div>
   );
--- sample-profile/src/Components/Dashboard/Dashboard.jsx
+++ sample-profile/src/Components/Dashboard/Dashboard.jsx
@@ -1,11 +1,48 @@
 import "./Dashboard.css";
-
+import { CgProfile } from "react-icons/cg";
+import { useState } from "react";
+import { BsCardText } from "react-icons/bs";
+import { IoWalletOutline } from "react-icons/io5";
+import { BiStar } from "react-icons/bi";
+import { BsBell } from "react-icons/bs";
+import { BiIdCard } from "react-icons/bi";
+import { IoExitOutline } from "react-icons/io5";
 const Dashboard = () => {
+  const [name, setName] = useState("عبدالله عبادی");
+  const [email, setEmail] = useState("Abdi.eb79@gmail.com");
+  const [dashboardItems, setDashboardItems] = useState([
+    { id: 1, title: "درخواست‌ها", src: <BsCardText /> },
+    { id: 2, title: "تراکنش مالی", src: <IoWalletOutline /> },
+    { id: 3, title: "امتیاز‌ها و نظرات", src: <BiStar /> },
+    { id: 4, title: "اعلان عا", src: <BsBell /> },
+    { id: 4, title: "تکمیل حساب کاربری", src: <BiIdCard /> },
+    { id: 4, title: "خروج", src: <IoExitOutline /> },
+  ]);
   return (
     <div className="dashboard">
       <div className="personal-info row">
-        <div className=""></div>
+        <div className="avatar col-xl-4">
+          <CgProfile size={30} />
+        </div>
+        <div className="info col-xl-8">
+          <div>{name}</div>
+          <div>{email}</div>
+        </div>
       </div>
+      <ul className="options">
+        {dashboardItems.map((item) => {
+          return (
+            <div
+              className="row d-flex justify-content-center align-items-center"
+              key={item.id}
+            >
+              <div className="icon col-2 align-items-center ">{item.src}</div>
+
+              <li className="col-8">{item.title}</li>
+            </div>
+          );
+        })}
+      </ul>
     </div>
   );
 };
